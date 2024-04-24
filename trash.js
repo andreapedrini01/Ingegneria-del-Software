db9c428f6ab981577674923aa4e1c00539418185
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2024-04-24 08:53:35 +0200

    added package and dependecies

--- trash.js
+++ trash.js
@@ -1,5 +1,10 @@
-function doSomething() {
-    if(1) {
-        int s = 1;
-    }
-}
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
