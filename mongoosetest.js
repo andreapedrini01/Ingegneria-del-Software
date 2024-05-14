f8f74977e43e90c4c9c843453026e4ed7a56016a
martinicomarco <marco.martinico@studenti.unitn.it>
2024-05-14 15:03:31 +0200

    updated

--- mongoosetest.js
+++ mongoosetest.js
@@ -3,6 +3,7 @@ const RegisteredUser = require('./models/RegisteredUser');
 const VerifiedUser = require('./models/VerifiedUser');
 const gruppo = require('./models/gruppo');
 const CentroRaccolta = require('./models/CentroRaccolta');
+const SuperUser = require('./models/SuperUser');
 
 mongoose.connect('mongodb+srv://marcomartinico:mongolia@cluster00.lfxhzww.mongodb.net/SpazzaTN', )
     .then(() => {
@@ -19,4 +20,26 @@ mongoose.connect('mongodb+srv://marcomartinico:mongolia@cluster00.lfxhzww.mongod
         password: "password",
         email: "ciao",
     });    
-    Ruser.save(); */
+    Ruser.save(); 
+
+    Suser = new SuperUser({
+
+        password: "SuperUser",
+        email: "superuser@admin",
+    });
+    Suser.save();
+    SuperUser.find()
+        .then((superusers) => {
+            console.log('Superusers:', superusers);
+        })
+        .catch((error) => {
+            console.error('Error querying superusers:', error);
+        });*/
+
+    RegisteredUser.find({username: "marco"})
+        .then((registeredusers) => {
+            console.log('Registeredusers:', registeredusers);
+        })
+        .catch((error) => {
+            console.error('Error querying registeredusers:', error);
+        });
