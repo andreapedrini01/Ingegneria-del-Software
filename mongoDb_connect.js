069d0091172b8a5ad29baa076ffeac4bb9f80146
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2024-05-14 14:54:59 +0200

    updated

--- mongoDb_connect.js
+++ mongoDb_connect.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = DB_URL;
+const uri = process.env.DB_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
