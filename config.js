b03f2b80720ee5f2056ccf146b06b31fa5788ab5
martinicomarco <marco.martinico@studenti.unitn.it>
2024-05-15 10:06:13 +0200

    installed dependencies

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
   dburl: process.env.DB_URL,
   masterKey: process.env.SUPER_SECRET,
   frontend: process.env.FRONTEND,
-  port: process.env.PORT
+  configPort: process.env.PORT
 };
