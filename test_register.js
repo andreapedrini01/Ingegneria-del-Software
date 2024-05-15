f79216e067f8caffd3a09060cb3137808688396f
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2024-05-15 15:55:06 +0200

    fetched function added

--- test_register.js
+++ test_register.js
@@ -1,34 +1,19 @@
 const mongoose = require('mongoose');
-const RegisteredUser = require('./models/registeredUser');
+const RegisteredUser = require('./app/models/registeredUser');
 
-// Function to save a registered user
-async function saveRegisteredUser(email, password, username) {
-    try {
-        // Create a new user instance
-        const user = new RegisteredUser({ email, password, username });
-        // Check if a user with the same email already exists in the database
-        const existingUser = await RegisteredUser.findOne({ email });
-
-        if (existingUser) {
-            console.log('User with the same email already exists');
-            return;
-        }
-
-        // Save the user to the database
-        const savedUser = await user.save();
-
-        console.log('User saved:', savedUser);
-    } catch (error) {
-        console.error('Error saving user:', error);
-    }
-}
-
-//connect to MongoDB
-mongoose.connect('mongodb+srv://marcomartinico:mongolia@cluster00.lfxhzww.mongodb.net/SpazzaTN', )
-.then(() => {
-    console.log('Connected to MongoDB');
-    saveRegisteredUser('example@example.com', 'password123', 'exampleUser');
+fetch('http://localhost:8080/register', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    email: 'sfsdf@fsvdv',
+    password: 'sfvsfg',
+    username: 'fiugetiug',
+  }),
 })
+.then(response => response.json())
+.then(data => console.log(data))
 .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+  console.error('Error:', error);
 });
