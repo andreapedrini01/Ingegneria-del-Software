b11f075fa19255cee5dc9b4a0e521b569874314b
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2024-05-15 18:21:24 +0200

    server refined

--- server.js
+++ server.js
@@ -2,39 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { dburl, configPort } = require('./config');
-const RegisteredUser = require('./app/models/registeredUser');
+
+const usersRouter = require('./app/users');
 
 const app = express();
 app.use(bodyParser.json());
-
-// Function to save a registered user
-async function saveRegisteredUser(email, password, username) {
-    try {
-        // Create a new user instance
-        const user = new RegisteredUser({ email, password, username });
-        // Check if a user with the same email already exists in the database
-        const existingUser = await RegisteredUser.findOne({ email });
-
-        if (existingUser) {
-            console.log('User with the same email already exists');
-            return;
-        }
-
-        // Save the user to the database
-        const savedUser = await user.save();
-
-        console.log('User saved:', savedUser);
-    } catch (error) {
-        console.error('Error saving user:', error);
-    }
-}
-
-// Endpoint to register a user
-app.post('/register', (req, res) => {
-    const { email, password, username } = req.body;
-    saveRegisteredUser(email, password, username);
-    res.send({ message: 'User registration successful' });
-});
+app.use('/users', usersRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello world');
