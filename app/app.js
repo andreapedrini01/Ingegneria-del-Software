97fad89e1502972865c59c0293642ed61855949d
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2024-05-15 22:59:38 +0200

    changed cors port

--- app/app.js
+++ app/app.js
@@ -28,7 +28,7 @@ app.use(cors())
 app.use(function (req, res, next) {
 
 //     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
 
 //     // Request methods you wish to allow
      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
