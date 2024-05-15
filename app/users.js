0e6de8c1ce2e2008f3b30520443f3e996ac398e4
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2024-05-15 18:22:18 +0200

    registration api added

--- app/users.js
+++ app/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./app/models/registeredUser'); // get our mongoose model
+const User = require('./models/registeredUser'); // get our mongoose model
+const { walk } = require('vue/compiler-sfc');
 
 router.get('/me', async (req, res) => {
     if(!req.loggedUser) {
@@ -11,7 +12,7 @@ router.get('/me', async (req, res) => {
     let user = await User.findOne({email: req.loggedUser.email});
 
     res.status(200).json({
-        self: '/api/v1/users/' + user.id,
+        self: '/app/users/' + user.id,
         email: user.email
     });
 });
@@ -27,7 +28,7 @@ router.get('', async (req, res) => {
 
     users = users.map( (entry) => {
         return {
-            self: '/api/v1/users/' + entry.id,
+            self: '/app/users/' + entry.id,
             email: entry.email
         }
     });
@@ -35,27 +36,41 @@ router.get('', async (req, res) => {
     res.status(200).json(students);
 });
 
-router.post('', async (req, res) => {
-    
-	let user = new User({
-        email: req.body.email,
-        password: req.body.password
-    });
+router.post('/register', async (req, res) => {
+    try{
+        let user = new User({
+            email: req.body.email,
+            password: req.body.password,
+            username: req.body.username
+        });
 
-    if (!user.email || typeof user.email != 'string' || !checkIfEmailInString(user.email)) {
-        res.status(400).json({ error: 'The field "email" must be a non-empty string, in email format' });
-        return;
+        if (!user.email || typeof user.email != 'string' || !checkIfEmailInString(user.email)) {
+            res.status(400).json({ error: 'The field "email" must be a non-empty string, in email format' });
+            return;
+        }
+
+        const existingUser = await User.findOne({ email: user.email });
+        //console.log('Existing user:', existingUser);
+        if (existingUser) {
+            res.status(409).json({ error: 'The email is already in use' });
+            console.log('User with the same email already exists, console log');
+            return;
+        }
+        console.log('Started waiting for save');
+        user = await user.save();
+        console.log('User saved:', user);
+        
+        let userId = user.id;
+
+        /**
+         * Link to the newly created resource is returned in the Location header
+         * https://www.restapitutorial.com/lessons/httpmethods.html
+         */
+        res.location("/app/users/" + userId).status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error('Error saving user:', error);
+        res.status(500).send();
     }
-    
-	user = await user.save();
-    
-    let userId = user.id;
-
-    /**
-     * Link to the newly created resource is returned in the Location header
-     * https://www.restapitutorial.com/lessons/httpmethods.html
-     */
-    res.location("/api/v1/users/" + userId).status(201).send();
 });
 
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
