de9d76c286cf9c2e9533886d8cec20db28ff5a4a
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2024-05-15 16:22:53 +0200

    variable user //ignore

--- test_register.js
+++ test_register.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 const RegisteredUser = require('./app/models/registeredUser');
 
+const user = {
+  email: 'carlomazzone@gmail.com',
+  password: 'password',
+  username: 'carlomazzone',
+}
+
 fetch('http://localhost:8080/register', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
   body: JSON.stringify({
-    email: 'sfsdf@fsvdv',
-    password: 'sfvsfg',
-    username: 'fiugetiug',
+    email: user.email,
+    password: user.password,
+    username: user.username,
   }),
 })
 .then(response => response.json())
