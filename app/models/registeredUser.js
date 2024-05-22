70244b43dd4986fde871f3268b8f14796dbfd45e
martinicomarco <marco.martinico@studenti.unitn.it>
2024-05-22 11:47:53 +0200

    aggiunto modello token

--- app/models/registeredUser.js
+++ app/models/registeredUser.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const bcryptSalt = process.env.BCRYPT_SALT;
 
 const registeredUserSchema = new mongoose.Schema({
     username:String,
@@ -19,7 +21,20 @@ const registeredUserSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'group'
     }]
-});
+},
+{
+    timestamps: true,
+  }
+);
+
+registeredUserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+      return next();
+    }
+    const hash = await bcrypt.hash(this.password, Number(bcryptSalt));
+    this.password = hash;
+    next();
+  });
 
 const registeredUser = mongoose.model('registeredUser', registeredUserSchema);
 
