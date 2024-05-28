d70dee5604e2e4326924785e57991c5e7265f3ec
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2024-05-28 11:35:28 +0200

    event schema

---
+++ app/models/event.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+// Definizione dello schema per gli eventi
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    startTime: {
+        type: String, // Conserva solo l'orario, ad esempio "08:00"
+        required: true
+    },
+    endTime: {
+        type: String, // Conserva solo l'orario, ad esempio "09:00"
+        required: true
+    },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    recurrenceDays: {
+        type: [String],
+        enum: ['lunedì', 'martedì', 'mercoledì', 'giovedì', 'venerdì', 'sabato', 'domenica'],
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+// Compilazione del modello
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
