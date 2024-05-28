beb4ff1b451a2aee88f739ee574df99c816d7fce
martinicomarco <marco.martinico@studenti.unitn.it>
2024-05-28 14:06:22 +0200

    updated

--- static/mappa.js
+++ static/mappa.js
@@ -6,15 +6,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([46.0665, 11.12]).addTo(map);
-marker.bindPopup("<b>hello world</b><br>I am a popup.").openPopup();
 
 fetch('api/v1/crm')
     .then(response => response.json())
     .then(data => {
         data.forEach(item => {
             var marker = L.marker([item.coordinates.longitudine, item.coordinates.latitudine]).addTo(map);
-            marker.bindPopup(item.descrizione).openPopup();
+            marker.bindPopup(item.descrizione);
         });
     })
     .catch(error => console.error('Error:', error));
