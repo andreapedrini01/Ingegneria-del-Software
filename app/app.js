c75449bafc6dd322e0671b910eb0d977e9b01feb
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2024-06-10 10:04:27 +0200

    deploy

--- app/app.js
+++ app/app.js
@@ -87,7 +87,9 @@ app.use((req,res,next) => {
     next()
 })
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'static', 'index.html'));
+  });
 
 /**
  * Authentication routing and middleware
